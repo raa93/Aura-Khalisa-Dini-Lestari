b9d7450ca5952691147e1bfac15b0ea4a44dfe2d
raa93 <146106393+raa93@users.noreply.github.com>
2023-12-08 19:51:43 +0700

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE todos (
+    id_pasien INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    poli VARCHAR(50) NOT NULL,
+    alamat VARCHAR(50) NOT NULL,
+    no_hp char(13) NOT NULL,
+    keluhan VARCHAR(50) NOT NULL
+);
